fdfe58e08ca618e3f193209725b55c3ffb7a17fb
Nathan Bolton <nathan@nathanbolton.ca>
2021-02-18 21:02:39 -0800

    remove redundant definitions

--- config/environments/development.php
+++ config/environments/development.php
@@ -10,10 +10,5 @@ Config::define('WP_DEBUG', true);
 Config::define('WP_DEBUG_DISPLAY', true);
 Config::define('WP_DISABLE_FATAL_ERROR_HANDLER', true);
 Config::define('SCRIPT_DEBUG', true);
-Config::define('FS_METHOD', 'direct');
-Config::define('WP_POST_REVISIONS', 3);
 
 ini_set('display_errors', '1');
-
-// Enable plugin and theme updates and installation from the admin
-Config::define('DISALLOW_FILE_MODS', false);
--- config/environments/staging.php
+++ config/environments/staging.php
@@ -8,6 +8,5 @@ use Roots\WPConfig\Config;
 Config::define('WP_DEBUG_DISPLAY', false);
 Config::define('SCRIPT_DEBUG', false);
 Config::define('DISALLOW_FILE_EDIT', true);
-Config::define('WP_POST_REVISIONS', 3);
 
 ini_set('display_errors', '0');
