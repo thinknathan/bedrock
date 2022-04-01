105825b95d0142addeba4b0ce523df3d4e2d388d
Nathan Bolton <nathan@nathanbolton.ca>
2022-04-01 15:01:44 -0700

    custom config

---
+++ config/environments/production.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Configuration overrides for WP_ENV === 'production'
+ */
+
+use Roots\WPConfig\Config;
+
+ini_set('display_errors', 0);
+Config::define('WP_DEBUG_DISPLAY', true);
+Config::define('SCRIPT_DEBUG', true);
+Config::define('DISALLOW_INDEXING', false);
--- config/environments/staging.php
+++ config/environments/staging.php
@@ -14,4 +14,7 @@ use Roots\WPConfig\Config;
  * Example: `Config::define('DISALLOW_FILE_MODS', false);`
  */
 
-Config::define('DISALLOW_INDEXING', true);
+ini_set('display_errors', 0);
+Config::define('WP_DEBUG_DISPLAY', true);
+Config::define('SCRIPT_DEBUG', true);
+Config::define('DISALLOW_INDEXING', false);
